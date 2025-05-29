c50756c034877199f19c9704c296cdb05ef1262f
Сметанин Евгений Владиславович <smetanin.evl@gazprom-neft.ru>
2025-05-29 22:52:27 +0300

    create application

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@Slf4j
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+}
