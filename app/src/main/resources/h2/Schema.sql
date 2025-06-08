59037bce057a1f019892c313d7f3943a562c6d7d
Сметанин Евгений Владиславович <smetanin.evl@gazprom-neft.ru>
2025-06-08 23:19:21 +0300

    исправил запрос

--- app/src/main/resources/h2/Schema.sql
+++ app/src/main/resources/h2/Schema.sql
@@ -1,8 +1,7 @@
 DROP TABLE IF EXISTS courses;
 
-CREATE TABLE url
-(
-    id         INT PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255),
+CREATE TABLE url (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
     created_at TIMESTAMP
 );
